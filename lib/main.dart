c589550b5684c1614bfa27da2814fcb1b164e909
meinayatraza <me.inayatraza@gmail.com>
2025-08-09 08:28:56 +0500

    JSON file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/app.dart';
+import 'data/repositories/data_repository.dart';
 
 void main() {
+  // Initialize DataRepository before running the app
+  Get.put(DataRepository());
   runApp(MyApp());
 }
